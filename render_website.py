c5c6ee0ef9f2ce84c764f0b81cc5a11843497ad3
Julia Kendo <ya1814@yandex.ru>
2020-08-17 12:07:59 +0300

    add paginator

--- render_website.py
+++ render_website.py
@@ -1,5 +1,6 @@
 import os
 import json
+import math
 from more_itertools import chunked
 from livereload import Server
 from jinja2 import Environment, FileSystemLoader, select_autoescape
@@ -23,9 +24,16 @@ def on_reload():
     template = env.get_template('template.html')
 
     books = read_books()
-    for books_group_number, books_group in enumerate(chunked(books, 20)):
-        rendered_page = template.render(books=books_group)
-        filename = os.path.join(pages_folder, f'index{books_group_number}.html')
+    pages = range(1, math.ceil(len(books) / 20) + 1)
+    filename = 'index.html'
+    for books_group_number, books_group in enumerate(chunked(books, 20), start=1):
+        rendered_page = template.render(
+            pages=pages,
+            books=books_group,
+            current_page=books_group_number
+        )
+        if books_group_number > 1:
+            filename = os.path.join(pages_folder, f'index{books_group_number}.html')
         with open(filename, 'w', encoding="utf8") as file:
             file.write(rendered_page)
 
